e1b9cedfe1ee004a88f68d95bed5ae85100ad152
Raj <rj.sse8@outlook.com>
2025-05-18 01:43:31 +0530

    Downgrade .NET version and remove OpenAPI integration
    Updated `deploy-to-azure.yml` to use .NET Core 8.0.x.
    Changed target framework in `Azure.AspNet.Web.Api.csproj` from
    `net9.0` to `net8.0` and removed OpenAPI package reference.
    In `Program.cs`, eliminated OpenAPI service configuration
    and related mappings, indicating a complete removal of
    OpenAPI support from the application.

--- Azure.AspNet.Web.Api/Program.cs
+++ Azure.AspNet.Web.Api/Program.cs
@@ -10,16 +10,8 @@ namespace Azure.AspNet.Web.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
 
             app.UseHttpsRedirection();
 
